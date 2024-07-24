3e1ed32a84de61f14b93c893a48df65e2429bfe6
JoonYong <joonyongpark99@gmail.com>
2024-07-24 17:27:22 +0900

    [INIT] Spring Boot project initial setup

---
+++ src/main/java/com/likelion/mindiary/MindiaryApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.mindiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/mindiary/MindiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.mindiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
