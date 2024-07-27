b43b61db8af8489430b007c7a5618638a0aace13
JoonYong <joonyongpark99@gmail.com>
2024-07-27 20:57:07 +0900

    #14 - [FEAT] Security ExceptionHandler 구현

---
+++ src/main/java/com/likelion/mindiary/global/Security/handler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,39 @@
+package com.likelion.mindiary.global.Security.handler;
+
+import static com.likelion.mindiary.global.error.CommonErrorCode.UNAUTHENTICATED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.likelion.mindiary.global.error.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+  private final ObjectMapper objectMapper;
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException authException) throws IOException, ServletException {
+    setResponse(response);
+  }
+
+  private void setResponse(HttpServletResponse response)
+      throws IOException {
+    response.setContentType(APPLICATION_JSON_VALUE);
+    response.setCharacterEncoding("UTF-8");
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+    objectMapper.writeValue(
+        response.getOutputStream(),
+        ErrorResponse.of(UNAUTHENTICATED));
+  }
+}
